a507154a010543df9fe3472b0a6cd8c6bef4c1ff
longyin <752299361@qq.com>
2019-03-21 21:33:53 +0800

    整合日志模块

--- test/src/main/java/com/zto/bigdata/spark/bean/Test.java
+++ test/src/main/java/com/zto/bigdata/spark/bean/Test.java
@@ -1,9 +1,17 @@
 package com.zto.bigdata.spark.bean;
 
-import static spark.Spark.get;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        List<String> list = Arrays.asList("1", "2", "3");
+        List<String> sub = list.subList(1, list.size());
+        Collections.shuffle(list);
+        for(String str : list) {
+            System.out.println(str);
+            break;
+        }
     }
 }
