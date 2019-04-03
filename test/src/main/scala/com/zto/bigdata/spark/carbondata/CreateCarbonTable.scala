c0c035fb20a38c7ef88f8e92df78719d504d9bb9
longyin <752299361@qq.com>
2019-04-03 21:19:44 +0800

    tispark测试

--- test/src/main/scala/com/zto/bigdata/spark/carbondata/CreateCarbonTable.scala
+++ test/src/main/scala/com/zto/bigdata/spark/carbondata/CreateCarbonTable.scala
@@ -1,11 +1,12 @@
 package com.zto.bigdata.spark.carbondata
 
 import com.zto.bigdata.spark.bean.Senda
-import com.zto.bigdata.spark.common.util.CarbondataUtils.buildCreateStreamingTableSQL
+import com.zto.bigdata.spark.common.util.CarbondataUtils
 
 object CreateCarbonTable {
 
   def main(args: Array[String]): Unit = {
-    buildCreateStreamingTableSQL("tmp", "dw_sz_zto_site_senda_bills", classOf[Senda])
+    // CarbondataUtils.buildCreateStreamingTableSQL("tmp", "dw_sz_zto_site_senda_bills", classOf[Senda])
+    CarbondataUtils.buildCreateTableSQL("tmp", "test2", classOf[Senda], null, true)
   }
 }
