18eabfd04b77b991f6b03798d022d81091ee9093
wangcl6 <wangchenglong@zto.com>
2019-10-24 16:10:23 +0800

    HBaseOper相关操作的batch支持配置

--- fire-core/src/main/scala/com/zto/fire/core/bridge/HBaseSparkBridge.scala
+++ fire-core/src/main/scala/com/zto/fire/core/bridge/HBaseSparkBridge.scala
@@ -4,7 +4,7 @@ import java.util
 
 import com.zto.fire.common.bean.HBaseBaseBean
 import com.zto.fire.common.db.HBaseOper
-import com.zto.fire.common.util.HBaseUtils
+import com.zto.fire.common.util.{GlobalConstants, HBaseUtils}
 import com.zto.fire.core.util.{SingletonFactory, SparkUtils}
 import org.apache.commons.lang3.StringUtils
 import org.apache.hadoop.hbase.client.{Get, Result, Scan}
@@ -26,7 +26,7 @@ import scala.reflect.ClassTag
   * @author ChengLong 2019-5-10 14:39:39
   */
 object HBaseSparkBridge extends HBaseOper with Logging {
-  val batchSize = 1000
+  lazy val batchSize = GlobalConstants.HBaseConf.hbaseBatchSize
 
   /**
     * 使用Java API的方式将DataFrame中的数据分多个批次插入到HBase中
