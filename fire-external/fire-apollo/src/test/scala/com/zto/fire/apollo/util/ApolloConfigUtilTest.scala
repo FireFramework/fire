3fa4f354f1295a398ddd796cf7e54a619ea191f6
wangcl6 <wangchenglong@zto.com>
2022-06-23 13:48:41 +0800

    [fire-core][feature] 屏蔽集群敏感信息

--- fire-external/fire-apollo/src/test/scala/com/zto/fire/apollo/util/ApolloConfigUtilTest.scala
+++ fire-external/fire-apollo/src/test/scala/com/zto/fire/apollo/util/ApolloConfigUtilTest.scala
@@ -24,13 +24,8 @@ object ApolloConfigUtilTest {
     //使用说明：默认使用读取dev环境的配置
     System.setProperty(ApolloConstant.APOLLO_ENV, "dev")
 
-    //可以通过如下参数直接设置属性值，否则根据 env环境配置读取 apollo的值
-    //System.setProperty("app.id", "fire1")
-    //System.setProperty("apollo.meta", "http://apollo.meta.dev.ztosys.com")
-
     println(ApolloConfigUtil.getProp)
     println(ApolloConfigUtil.getInt("test"))
-
   }
 
 }
