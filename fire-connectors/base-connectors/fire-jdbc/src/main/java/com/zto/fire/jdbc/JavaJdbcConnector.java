4412f6ff70ef86e33fc3f27b08a8cfa2e58e67d1
wangcl6 <wangchenglong@zto.com>
2025-11-27 14:51:37 +0800

    [fire-1262] jdbc connector支持java调用

--- fire-connectors/base-connectors/fire-jdbc/src/main/java/com/zto/fire/jdbc/JavaJdbcConnector.java
+++ fire-connectors/base-connectors/fire-jdbc/src/main/java/com/zto/fire/jdbc/JavaJdbcConnector.java
@@ -17,5 +17,32 @@
 
 package com.zto.fire.jdbc;
 
-public class JavaJdbcConnectorBridge {
+import java.sql.Connection;
+
+/**
+ * Java版本的JDBC连接器，用于封装Scala的JdbcConnector
+ *
+ * @author chenglong 2025-11-27 14:32:52
+ * @since 2.6.3
+ */
+public class JavaJdbcConnector {
+
+    /**
+     * 获取jdbc连接
+     * @param keyNum
+     * 对应配置文件的key值
+     * @return
+     * 数据库连接
+     */
+    public static Connection getConnection(int keyNum) {
+        // 这里是获取数据库连接的具体实现，例如使用JDBC驱动
+        return JdbcConnector$.MODULE$.getConnection(keyNum);
+    }
+
+    /**
+     * 关闭指定的数据库连接
+     */
+    public static void closeConnection(Connection conn) {
+        JdbcConnector$.MODULE$.closeConnection(conn);
+    }
 }
