2d7836d979635e14e5d69f885852defc815a1356
wangcl6 <wangchenglong@zto.com>
2026-06-09 13:20:59 +0800

    [fire-1318] 代码动态扫描json配置改为base64加密，解决分发到TaskManager端配置被截断的问题

--- fire-engines/fire-flink/src/main/scala/com/zto/fire/flink/task/FlinkInternalTask.scala
+++ fire-engines/fire-flink/src/main/scala/com/zto/fire/flink/task/FlinkInternalTask.scala
@@ -57,16 +57,10 @@ private[fire] class FlinkInternalTask(baseFlink: BaseFlink) extends FireInternal
    */
   @Scheduled(fixedInterval = 120000, initialDelay = 60000)
   def standard: Unit = {
-    println("--------------1")
-    println("FireFrameworkConf.traceCodeStandardEnable=" + FireFrameworkConf.traceCodeStandardEnable)
-    println("FireFrameworkConf.traceCodeStandardSendMqUrl=" + FireFrameworkConf.traceCodeStandardSendMqUrl)
-    println("StringUtils.isNotBlank(FireFrameworkConf.traceCodeStandardSendMqTopic)=" + StringUtils.isNotBlank(FireFrameworkConf.traceCodeStandardSendMqTopic))
     if (FireFrameworkConf.traceCodeStandardEnable && StringUtils.isNotBlank(FireFrameworkConf.traceCodeStandardSendMqUrl) && StringUtils.isNotBlank(FireFrameworkConf.traceCodeStandardSendMqTopic)) {
       val standards = FlinkStandardAccumulatorManager.getAndReset
-      println("standards=" + standards)
       if (standards != null && !standards.isEmpty) {
         val standardJson = JSONUtils.toJSONString(standards)
-        println("standardJson=" + standardJson)
         logWarning("向kafka发送代码规范检测json：" + standardJson)
         MQProducer.sendKafka(FireFrameworkConf.traceCodeStandardSendMqUrl, FireFrameworkConf.traceCodeStandardSendMqTopic, standardJson, throwable = false)
       }
